659ee4c45d1be0c1d63b1b9f8cb07f64dea19f71
Noseh Taghaymurodzoda <nosehtagaymurodzoda@gmail.com>
2025-09-16 14:01:39 +0500

    .

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class User
+{
+    [Required]
+    [StringLength(50, MinimumLength = 3,ErrorMessage = "First name must be between 3 and 50 characters")]
+    public string FirstName { get; set; } = string.Empty;
+    [StringLength(50, MinimumLength = 3,ErrorMessage = "Last name must be between 3 and 50 characters")]
+    public string? LastName { get; set; }
+    public bool IsDeleted { get; set; } = false;
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedAt { get; set; }  = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
